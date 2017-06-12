eb80a170297bbb0ee279dd3397cf5727b6a2df85
Clemens Lange <clemens.lange@cern.ch>
2017-06-12 16:56:06 +0200

    declare use of LHAPDF

--- Analysis/MssmHbb/src/JetAnalysisBase.cpp
+++ Analysis/MssmHbb/src/JetAnalysisBase.cpp
@@ -8,6 +8,9 @@
 #include <chrono>
 #include "Analysis/MssmHbb/interface/JetAnalysisBase.h"
 
+// LHAPDF
+#include "LHAPDF/LHAPDF.h"
+
 using namespace analysis;
 using namespace analysis::tools;
 using namespace analysis::mssmhbb;
