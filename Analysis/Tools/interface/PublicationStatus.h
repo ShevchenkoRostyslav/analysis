cbdff8416eb1ef48abf85ca911a05ea3e519746a
Rostyslav Shevchenko <shevchenko.rostislav@gmail.com>
2018-08-21 13:37:34 +0200

    Publication Status

---
+++ Analysis/Tools/interface/PublicationStatus.h
@@ -0,0 +1,19 @@
+/*
+ * PublicationStatus.h
+ *
+ *  Created on: 11 Jan 2018
+ *      Author: shevchen
+ */
+
+#ifndef ANALYSIS_TOOLS_INTERFACE_PUBLICATIONSTATUS_H_
+#define ANALYSIS_TOOLS_INTERFACE_PUBLICATIONSTATUS_H_
+
+#include <string>
+
+// Publication status: determines what is plotted in title
+enum PublicationStatus { INTERNAL, INTERNAL_SIMULATION, PRELIMINARY, PUBLIC, SIMULATION, UNPUBLISHED, PRIVATE, PRELIMINARY_SIMULATION, WORKINPROGRESS, SUPPLEMENTARY,PRIVATE_SIMULATION };
+std::string PublicationStatusToString(const PublicationStatus status);
+
+
+
+#endif /* ANALYSIS_TOOLS_INTERFACE_PUBLICATIONSTATUS_H_ */
